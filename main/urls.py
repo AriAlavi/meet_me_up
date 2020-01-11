7bdd34eb8ec7653b48d56421d2ca0ae35bab7d8b
Christian Santos <cesantos647@gmail.com>
2020-01-11 05:33:22 -0800

    added busy page

--- main/urls.py
+++ main/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path("", main_views.home, name="home"),
     path("login/", auth_views.LoginView.as_view(template_name="main/login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name="main/logout.html"), name="logout"),
-    path('register/', main_views.register, name="reigster"),
+    path('register/', main_views.register, name="register"),
     path('accounts/profile/', main_views.profile, name="profile_redirect"),
     path('index/', main_views.index, name = "index"),
-    path('event/<str:code_name>/', main_views.event, name = "event")
+    path('event/<str:code_name>/', main_views.event, name = "event"),
+    path('busy/', main_views.busy, name = "busy")
 ]
