bd6cc93311cca9608a1c121e5d2c9c66cffcde44
AriAlavi <arianalavi1@gmail.com>
2020-01-11 05:23:02 -0800

    Getfree functions

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from main.models import Event, Profile
+from main.models import Event, Profile, Busy
 
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Profile)
+admin.site.register(Busy)
