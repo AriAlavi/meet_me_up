8e3eb64162aaaed84833fb3d567ad0f53aaf0e51
AriAlavi <arianalavi1@gmail.com>
2020-01-10 23:09:51 -0800

    Added static root

--- meet_me_up/settings.py
+++ meet_me_up/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 SECRET_FILE.close()
 SECRET_FILE = open(SECRET_FILE_PATH, "w+")
