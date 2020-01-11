250ffdcc05cd665e599081b6f3d45af8a03cc071
AriAlavi <arianalavi1@gmail.com>
2020-01-11 00:08:59 -0800

    Added register and login system

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-import main.views as views
+from django.contrib.auth import views as auth_views
+import main.views as main_views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", main_views.home, name="home"),
+    path("login/", auth_views.LoginView.as_view(template_name="main/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="main/logout.html"), name="logout"),
+    path('register/', main_views.register, name="reigster"),
+    path('accounts/profile/', main_views.profile, name="profile_redirect")
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound
+from django.shortcuts import redirect
 
-# Create your views here.
+from main.models import Profile
 
 def home(request):
     context = {}
     return render(request, "main/home.html", context)
+
+def profile(request):
+    messages.success(request, "Logged in successfully!")
+    return redirect('home')
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    if request.POST:
+        if form.is_valid():
+            user = form.save()
+            profile = Profile()
+            profile.user = user
+            profile.save()
+            return redirect('home')
+        else:
+            for error in form.errors.items():
+                messages.warning(request, error)
+
+    context = {
+        'form' : form
+    }
+    return render(request, "main/register.html", context)
