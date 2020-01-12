9f892934fa22818faf4bd71a2d1365e804dd27d2
AriAlavi <arianalavi1@gmail.com>
2020-01-12 04:50:33 -0800

    opacity

--- main/views.py
+++ main/views.py
@@ -124,7 +124,7 @@ def freeInterface(request):
 
         return HttpResponseBadRequest("{} is not declared".format(data_type))
     else:
-        DATA_TYPES = ["getFree", "getEventFree"]
+        DATA_TYPES = ["getFree", "getEventFree", "getEventHeatmap"]
         try:
             data_type = request.GET["data_type"]
         except:
@@ -132,7 +132,10 @@ def freeInterface(request):
         if data_type not in DATA_TYPES:
             return HttpResponseBadRequest("{} is not a valid data type. Pick from: {}".format(data_type, DATA_TYPES))
         
-        if data_type == "getFree" or data_type == "getEventFree":
+
+
+
+        if data_type == "getFree":
             try:
                 start_date = request.GET['start_date']
                 end_date = request.GET['end_date']
@@ -143,25 +146,26 @@ def freeInterface(request):
                 end_date = datetime.strptime(end_date, "%m/%d/%Y")
             except:
                 return HttpResponseBadRequest("dates must be in m/d/Y form")
-
-            if data_type == "getFree":
-                return HttpResponse(json.dumps(request.user.profile.getFreeArray(start_date, end_date)))
-            elif data_type == "getEventFree":
-                try:
-                    event = request.GET['event']
-                except:
-                    return HttpResponseBadRequest("event argument required")
-                try:
-                    event = Event.objects.get(code_name=event)
-                except:
-                    return HttpResponseNotFound("{} is not a valid event".format(event))
+            return HttpResponse(json.dumps(request.user.profile.getFreeArray(start_date, end_date)))
+        elif data_type == "getEventFree" or data_type == "getEventHeatmap":
+            try:
+                event = request.GET['event']
+            except:
+                return HttpResponseBadRequest("event argument required")
+            try:
+                event = Event.objects.get(code_name=event)
+            except:
+                return HttpResponseNotFound("{} is not a valid event".format(event))
+            if data_type == "getEventFree":
                 results = {}
                 for profile in event.profile_set.all():
-                    results[profile.id] = profile.getFreeArray(start_date, end_date)
+                    results[profile.id] = profile.getFreeArray(event.start_date, event.end_date)
                 return HttpResponse(json.dumps(results))
+            elif data_type == "getEventHeatmap":
+                return HttpResponse(json.dumps(event.getOverlapHeatMap()))
 
-            else:
-                raise Exception("Logic error")
+        else:
+            raise Exception("Logic error")
 
         return HttpResponseBadRequest("{} is not declared in the interface".format(data_type))    
 
