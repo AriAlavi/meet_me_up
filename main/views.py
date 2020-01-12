12ccb9c80dacbe209d16e27a039b2ea722fa8801
Christian Santos <cesantos647@gmail.com>
2020-01-11 16:29:31 -0800

    created pathway to create an event

--- main/views.py
+++ main/views.py
@@ -109,3 +109,10 @@ def busy(request):
 
     }
     return render(request, "main/busy.html", context)
+
+@login_required
+def create(request):
+    context = {
+
+    }
+    return render(request, "main/create.html", content)
